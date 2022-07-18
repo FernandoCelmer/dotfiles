db329439411f63267565a94912ecf33d50d1c376
FernandoCelmer <email@fernandocelmer.com>
2022-07-18 20:01:44 -0300

    No Task - Script Install vim

---
+++ setup/install_vim.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "INSTALL: vim"
+sudo apt install vim -y
+
+array=(
+	"vimrc"
+	"vimrc.local"
+	".vimrc.plug"
+)
+
+echo "INSTALL: plug.vim"
+sudo curl -fLo /etc/vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+for file in ${array[@]}; do
+	echo "COPY: $file"
+	sudo cp ../vim/$file /etc/vim/$file
+done
+
+if [ ! -f "/etc/vim/plugged" ]; then
+	echo "CREATE: plugged"
+	sudo mkdir /etc/vim/plugged
+fi
+
+
+if [ ! -f "/etc/vim/vimplug-plugins" ]; then
+	echo "CREATE: vimplug-plugins"
+	sudo mkdir /etc/vim/vimplug-plugins
+fi
+
