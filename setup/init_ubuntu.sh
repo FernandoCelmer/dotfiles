b07aa1a38ba634b4bb170435fb6a7f204c85dee6
FernandoCelmer <email@fernandocelmer.com>
2022-07-18 17:45:22 -0300

    No Task - Init Ubuntu

--- setup/init_ubuntu.sh
+++ setup/init_ubuntu.sh
@@ -1,1 +1,29 @@
-sudo apt install git
+#!/bin/bash
+
+INPUT=packages_ubuntu.csv
+OLDIFS=$IFS
+IFS=','
+
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read type name category url
+do
+	if [ "$type" == "APT" ]
+	then
+		echo "APT: $name"
+
+   	elif [ "$type" == "SNAP" ]
+	then
+		echo "SNAP: $name"
+
+	elif [ "$type" == "DEB" ]
+	then
+		echo "DEB: $name"	
+	
+	elif [ "$type" == "SH" ]
+	then
+		echo "SH: $name"
+	fi
+
+done < $INPUT
+IFS=$OLDIFS
+
