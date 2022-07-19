f7b7ff5602f20f32699b7e57fb374038c64cf8da
FernandoCelmer <email@fernandocelmer.com>
2022-07-18 21:37:13 -0300

    No Task - Init Ubuntu

--- setup/init_ubuntu.sh
+++ setup/init_ubuntu.sh
@@ -6,31 +6,39 @@ INPUT=packages_ubuntu.csv
 OLDIFS=$IFS
 IFS=','
 
-sudo apt update -y
-sudo apt upgrade -y
+# sudo apt update -y
+# sudo apt upgrade -y
 
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read type name category url
+while read type name category url file
 do
 	if [ "$type" == "apt" ]
 	then
 		echo "INSTALL: $name"
 		sudo $type install $name -y
-	
+		echo "=========="
+
 	elif [ "$type" == "snap" ]
 	then
 		echo "INSTALL: $name"
 		sudo $type install $name
+		echo "=========="
 
 	elif [ "$type" == "deb" ]
 	then
-		echo "INSTALL: $name"	
+		INSTALL: $name
+		wget $url
+		sudo dpkg -i $file
+		sudo apt install -f
+		sudo rm -r $file
+		echo "=========="
 	
 	elif [ "$type" == "sh" ]
 	then
 		echo "INSTALL: $name"
-		sudo chmod 777 $url
-		sudo ./$url
+		sudo chmod 777 $file
+		sudo ./$file
+		echo "=========="
 	fi
 
 done < $INPUT
