b9202dcd7f768b395052ac5cf51e4cec58882e61
FernandoCelmer <email@fernandocelmer.com>
2022-07-18 23:49:25 -0300

    No Task - Script Install terminal

---
+++ setup/install_terminal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+array=(
+        "Konsole.colorscheme"
+        "Konsole.profile"
+)
+
+for file in ${array[@]}; do
+        echo "COPY: $file"
+        sudo cp ../terminal/$file ~/.local/share/konsole/$file
+done
