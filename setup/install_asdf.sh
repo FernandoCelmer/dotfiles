a85cc6f3932389cdd50c72862dcb8f585c55dbcc
FernandoCelmer <email@fernandocelmer.com>
2022-07-19 14:38:37 -0300

    No Task - Script Install asdf

--- setup/install_asdf.sh
+++ setup/install_asdf.sh
@@ -10,14 +10,14 @@ echo ". ""$""HOME/.asdf/asdf.sh" >> ~/.bashrc
 echo ". ""$""HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
 
 # PYTHON
-echo "INSTALL: Python"
+echo "INSTALL: python"
 asdf plugin-add python
 #asdf install python 2.7.18
 #asdf install python 3.6.12
 echo "=========="
 
 # NODEJS
-echo "INSTALL: NodeJS"
+echo "INSTALL: nodejs"
 asdf plugin-add nodejs
 #asdf install nodejs 12.10.0
 #asdf install nodejs 14.16.0
@@ -25,6 +25,7 @@ asdf plugin-add nodejs
 echo "=========="
 
 # PHP
+echo "INSTALL: php"
 asdf plugin-add php
 #asdf install php 8.0.0
 echo "=========="
